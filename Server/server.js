58b9205e7faf01cacaf70855766acfd364e3af27
Farheen khan <farheenkhancc@gmail.com>
2022-12-17 21:48:48 +0530

    Setup transaction API

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,45 @@
-import  express  from "express";
+import express from "express";
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import Transaction from "./models/Transaction.js";
 const PORT = 4000;
 const app = express();
 
-app.get("/" , (req , res) => {
+
+app.use(cors());
+
+
+await mongoose.connect('mongodb+srv://expenser-project:expenser123@mern-project.2z9y0ob.mongodb.net/?retryWrites=true&w=majority')
+console.log('your DB is Connected')
+
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
     res.send("Hellow my world")
 })
 
-app.use(cors) ; 
-await mongoose.connect('mongodb+srv://expenser-project:expenser123@mern-project.2z9y0ob.mongodb.net/?retryWrites=true&w=majority')
-console.log('your DB is Connected')
+app.get("/transaction" , async (req , res) => {
+  const transaction = await Transaction.find({}).sort({createdAt : -1});
+  res.json({data : transaction});
+});
+
+
+
+app.post("/transaction", async (req , res) => {
+    const {amount , description , date} = req.body;
+
+    const transaction = new Transaction( {
+        amount , 
+        description, 
+        date,
+    })
+    await transaction.save();
+    res.json({
+        message: "SucessFull",
+    });
+})
+
 
 app.listen(PORT, () => {
     console.log("Server is running Aat http://localhost:4000")
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
 
-import {useState} from 'react' ; 
+import {useEffect, useState} from 'react' ; 
 
 function App() {
   const [form, setForm] = useState({
-    amount : 0, 
+     amount : 0, 
      description : "" , 
-     date : " ",
-  }) 
+     date : new Date(),
+  }); 
+
+  const [transactions, setTransactions] = useState([])
+  
+  useEffect(() => {
+    fetchTransactions();
+  }, [])
+  
+  async function fetchTransactions() {
+    const res = await fetch("http://localhost:4000/transaction");
+    const {data }= await res.json();
+    setTransactions(data)
+    console.log(data)
+  }
 
   async function handleSubmit(e) {
     e.preventDefault()
     console.log(form);
     const res = await fetch("http://localhost:4000/transaction" , {
       method : "POST", 
-      body : form
-    });
-    console.log(res)
+      body : JSON.stringify(form),
+      headers : {
+        "content-type" : "application/json"
+      }
+    }); 
+    if(res.ok) {
+      fetchTransactions()
+    }
   }
 
   function handleChange(e) {
      setForm({...form , [e.target.name]:e.target.value})
-     console.log(e.target.value)
   }
   return (
     <div>
@@ -30,6 +47,28 @@ function App() {
       <input type="date"   name="date" value={form.date} onChange={handleChange}/>
       <button type="submit">Submit</button>
       </form>
+      <br /> 
+      
+      <section>
+        <table>
+          <thead>
+            <tr>
+            <th>Amount</th>
+            <th>Description</th>
+            <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((trx) => (
+              <tr key={trx._id}>
+               <td>{trx.amount}</td>
+               <td>{trx.description}</td>
+               <td>{trx.date}</td>
+               </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
     </div>
   )
 }
